373b98c27dfc2f64da266f364d1642dc56afa26a
Matthias Kausl <matthias.kausl@willhaben.at>
2015-08-28 10:07:30 +0200

    added angular

--- src/main/java/at/willhaben/aws/test/Main.java
+++ src/main/java/at/willhaben/aws/test/Main.java
@@ -2,6 +2,12 @@ package at.willhaben.aws.test;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.util.StringInputStream;
+
+import java.io.UnsupportedEncodingException;
 
 public class Main {
 
@@ -14,7 +20,12 @@ public class Main {
     }
 
 
-    public String myHandler(int myCount, Context context) {
+    public String myHandler(int myCount, Context context) throws UnsupportedEncodingException {
+        AmazonS3Client amazonS3Client = new AmazonS3Client();
+        amazonS3Client.putObject("wh-template-test",myCount+".txt",new StringInputStream("asdf"), new ObjectMetadata());
+
+
+
         LambdaLogger logger = context.getLogger();
         logger.log("received : " + myCount);
         return String.valueOf(myCount);
