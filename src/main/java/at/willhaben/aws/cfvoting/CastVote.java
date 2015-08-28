56c66f5f7076e67ce38760c50895f3b2e8248c5c
Matthias Kausl <matthias.kausl@willhaben.at>
2015-08-28 12:16:50 +0200

    castVoteResource

--- src/main/java/at/willhaben/aws/cfvoting/CastVote.java
+++ src/main/java/at/willhaben/aws/cfvoting/CastVote.java
@@ -1,5 +1,14 @@
 package at.willhaben.aws.cfvoting;
 
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.spec.UpdateItemSpec;
+import com.amazonaws.services.dynamodbv2.document.utils.NameMap;
+import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 
@@ -12,7 +21,7 @@ import java.io.UnsupportedEncodingException;
 public class CastVote {
 
     public static void main(String[] args) {
-
+        new CastVote().vote(new Vote("12345", "test"));
     }
 
     public CastVote() {
@@ -24,7 +33,30 @@ public class CastVote {
 
         LambdaLogger logger = context.getLogger();
         logger.log("received : " + vote);
+
+        vote(vote);
+
         return String.valueOf(vote.getVote());
+    }
+
+    private void vote(Vote vote) {
+        AmazonDynamoDBClient client = new AmazonDynamoDBClient();
+        client.setRegion(Region.getRegion(Regions.EU_WEST_1));
+        DynamoDB dynamoDB = new DynamoDB(client);
+
+
+        Table table = dynamoDB.getTable("wh-voting");
+
+        Item item = table.getItem("token", vote.getTokenId());
+
+        UpdateItemSpec updateItemSpec = new UpdateItemSpec()
+                .withPrimaryKey("token", vote.getTokenId())
+                .withUpdateExpression("set #vote = :vote")
+                .withNameMap(new NameMap().with("#vote", "vote"))
+                .withValueMap(new ValueMap().withString(":vote", vote.getVote()));
+
+        table.updateItem(updateItemSpec);
+
     }
 
 }
--- src/main/java/at/willhaben/aws/cfvoting/GetOptions.java
+++ src/main/java/at/willhaben/aws/cfvoting/GetOptions.java
@@ -1,5 +1,12 @@
 package at.willhaben.aws.cfvoting;
 
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.Table;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.s3.AmazonS3Client;
@@ -7,6 +14,7 @@ import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.util.StringInputStream;
 
 import java.io.UnsupportedEncodingException;
+import java.util.List;
 
 /**
  * AWS Lambda Class for getting voting options for one voting token
@@ -14,7 +22,11 @@ import java.io.UnsupportedEncodingException;
 public class GetOptions {
 
     public static void main(String[] args) {
+        GetOptions getOptions = new GetOptions();
 
+        String[] options = getOptions.getOptions("12345a");
+
+        System.out.print(options);
     }
 
     public GetOptions() {
@@ -25,7 +37,40 @@ public class GetOptions {
     public String[] myHandler(String tokenId, Context context) throws UnsupportedEncodingException {
 
         LambdaLogger logger = context.getLogger();
-        return new String[]{"a", "b"};
+        logger.log("get options for " + tokenId);
+        return getOptions(tokenId);
+    }
+
+    private String[] getOptions(String tokenId) {
+
+
+        AmazonDynamoDBClient client = new AmazonDynamoDBClient();
+        client.setRegion(Region.getRegion(Regions.EU_WEST_1));
+        DynamoDB dynamoDB = new DynamoDB(client);
+
+
+        Table table = dynamoDB.getTable("wh-voting");
+
+        Item item = table.getItem("token", tokenId);
+
+        if (item == null) throw new IllegalArgumentException();
+
+        Object options = item.asMap().get("options");
+
+        if (options instanceof List) {
+            List list = (List) options;
+
+            String[] objects = new String[list.size()];
+            int i = 0;
+            for (Object option : list.toArray()) {
+                objects[i++] = (String) option;
+            }
+            return objects;
+        } else {
+            throw new IllegalArgumentException();
+        }
+
+
     }
 
 }
