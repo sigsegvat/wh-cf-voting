19cb4b127b6ab0755724a9066be6879324ed0409
whreseil <reinhard.seiler@willhaben.at>
2015-08-28 11:41:11 +0200

    angular init setup (not working)

--- src/main/resources/js/main.js
+++ src/main/resources/js/main.js
@@ -1,1 +1,44 @@
-console.log("test");
+angular.module('whVotingApp', [
+    'ngRoute',
+    'indexController',
+    'resultController'
+]);
+
+whVotingApp.config(['$routeProvider',
+        function($routeProvider) {
+            $routeProvider.
+                when('/', {
+                    templateUrl: 'tpl/index.tpl.html',
+                    controller: 'indexController'
+                }).
+                when('/result', {
+                    templateUrl: 'tpl/result.tpl.html',
+                    controller: 'resultController'
+                })
+                .otherwise({
+                    redirectTo: '/'
+                })
+            ;
+        }]
+);
+
+whVotingApp.run(function ($rootScope, $location) {
+    $rootScope.$on("$routeChangeError", function () {
+        $location.path("/");
+        alert(1);
+    });
+});
+
+var indexController = angular.module('indexController', []);
+indexController.controller('indexController', ['$scope',
+    function($scope) {
+        alert(1);
+    }])
+;
+
+var resultController = angular.module('resultController', []);
+resultController.controller('resultController', ['$scope',
+    function($scope) {
+        alert(2);
+    }])
+;
